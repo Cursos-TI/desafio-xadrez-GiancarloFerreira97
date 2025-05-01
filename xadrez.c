1655ae76b6137529e9d0d7262afb6bb67fd3b0eb
Giancarlo Ferreira Amorim <giancarlo.ferreira@hotmail.com>
2025-04-30 22:03:16 -0300

    Update xadrez.c
    Xadrez - Desafio Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,14 @@
 #include <stdio.h> 
 
 int main (){
-//Xadrez - Desafio Nível Novato
-//Torre, Bispo e Rainha
-//For, While, Do-While
+//Xadrez - Desafio Nível Aventureiro
+//Torre, Bispo, Rainha e cavalo
+//For, While, Do-While & Loop Aninhado
 
 //Torre = 5 casas para a direita
 //Bispo = 5 casas na diagonal para cima e à direita (Cima,Direita)
 //Rainha = 8 casas para a esquerda
+//Cavalo = 2 casas para baixo e 1 casa para a esquerda
 
 //For
     int TorreAcao = 5; //Definição da variável para o movimento e número de casas movimentadas
@@ -34,10 +35,9 @@ int main (){
     
 
 //Do-While
-    int RainhaAcao = 8; //futuro
-    int CasaRainha = 0; //atual
-
     printf("\nMovimento da Rainha: \n");
+    int RainhaAcao = 8; //futuro
+    int CasaRainha = 0; //atual 
 
     do {
         printf("Esquerda.\n"); //repetição
@@ -45,5 +45,23 @@ int main (){
     } while (CasaRainha < RainhaAcao); //condição
 
 
+//Loop Aninhado (For & While)
+    printf("\nMovimento do Cavalo: \n");
+    int CavaloAcao1 = 2; //Baixo
+    int CavaloAcao2 = 1; //Esquerda
+
+
+    for (i = 0; i < CavaloAcao2; i++)
+    {
+        while (i < CavaloAcao1)
+        {
+            i++;
+            printf("Baixo.\n");
+        }
+        
+        printf("Esquerda.\n");
+    }
+
+    
     return 0;
 }
